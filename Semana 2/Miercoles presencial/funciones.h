c244fd000063fc9cf6a8e6579dd5d170c4c13643
MaximilianoWenner <88354878+MaximilianoWenner@users.noreply.github.com>
2025-08-21 08:27:41 -0300

    Add files via upload

---
+++ Semana 2/Miercoles presencial/funciones.h
@@ -0,0 +1,9 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+
+void menu();
+int *crearVector(int &);
+void cargarVector(int *, int);
+void mostrarVector(int *, int);
+
+#endif // FUNCIONES_H_INCLUDED
