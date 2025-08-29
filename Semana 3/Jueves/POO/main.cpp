ece95639586b1b96e61e51d20407f0c1304aba02
Brian Lara <brian.lara@outlook.com.ar>
2025-08-29 12:27:14 -0300

    Semana 3

---
+++ Semana 3/Jueves/POO/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+/****
+  
+  una forma de resolver problemas utilizando objetos
+  Paradigma programacion orientada a objetos
+  
+  paradigmas
+    imperativos (como?)
+      estructurado
+      procedural
+      orientado a objetos <------
+      
+    declarativos (que?)
+      funcional -> javascript
+      logico -> IA -> SI
+      React JSX-> declarativo!
+      
+  Que es un Objeto?
+  La abstraccion de un objeto del mundo real, obtienen caracteristicas y acciones.
+  
+  Que es un objeto del mundo real?
+  Es una entidad real o imaginaria, tangible o intangible 
+  
+  Que tiene un objeto en programacion?
+  Estados : Son los valores de sus propiedades (memoria)
+  Comportamiento : Lo que puede hacer el objeto (metodos) - Mensajes
+  Indentidad : El nombre
+
+  Que es encapsulamiento?
+  Ocultar el funcionamiento interno, y proveer una interface   
+
+  Como resolvemos un problema?
+  
+  Identificar el problema
+  Identificar los objetos --> propiedes 
+  Identifican sus interacciones --> metodos
+  Clasificar los objetos  --> clases
+  Generalizas las clases --> herencia
+  
+  /// como hago las clases?
+  class NombreClase {
+    private: ocultar
+    public: interface
+  }
+*/
+
+
+int main()
+{
+    
+    return 0;
+}
