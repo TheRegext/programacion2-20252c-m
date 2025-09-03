0c110fd689b4d15e810dc17b784ec0de560d9c48
MaximilianoWenner <88354878+MaximilianoWenner@users.noreply.github.com>
2025-09-03 12:01:31 -0300

    Add files via upload

---
+++ Semana 4/Miercoles presencial/clsDomicilio.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstring>
+#include "cargarCadena.h"
+#include "clsDomicilio.h"
+
+using namespace std;
+
+void Domicilio::Cargar(){
+    cout<<"INGRESE EL NOMBRE DE LA CALLE: ";
+    cargarCadena(calle,29);
+    cout<<"INGRESE LA ALTURA: ";
+    cin>>altura;
+    cout<<"INGRESE EL CODIGO POSTAL: ";
+    cargarCadena(codigoPostal,5);
+    cout<<"INGRESE LA LOCALIDAD: ";
+    cargarCadena(localidad,29);
+    cout<<"INGRESE EL PARTIDO: ";
+    cargarCadena(partido,29);
+    cout<<"INGRESE LA PROVINCIA: ";
+    cargarCadena(provincia,29);
+}
+
+void Domicilio::Mostrar(){
+    cout<<"CALLE:"<<calle<<endl;
+    cout<<"ALTURA:"<<altura<<endl;
+    cout<<"CODIGO POSTAL:"<<codigoPostal<<endl;
+    cout<<"LOCALIDAD:"<<localidad<<endl;
+    cout<<"PARTIDO:"<<partido<<endl;
+    cout<<"PROVINCIA:"<<provincia<<endl;
+}
