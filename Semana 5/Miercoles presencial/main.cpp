bb793320d6b4043bf1dc08b013a539e6effa0672
MaximilianoWenner <88354878+MaximilianoWenner@users.noreply.github.com>
2025-09-10 11:03:56 -0300

    Add files via upload

---
+++ Semana 5/Miercoles presencial/main.cpp
@@ -0,0 +1,113 @@
+#include <iostream>
+#include <cstring>
+#include "funciones.h"
+#include "clsContador.h"
+#include "clsAlumno.h"
+
+using namespace std;
+
+/**
+DESARROLLAR UNA CLASE Fecha, QUE PERMITA TRABAJAR CON LAS
+SIGUIENTES PROPIEDADES:
+-DIA, MES, AÑO.
+AGREGAR LOS SETTERS y GETTERS CORRESPONDIENTES.
+LOS OBJETOS DE FECHA DEBEN ESTABLECER VALORES A LAS PROPIEDADES
+<<AL MOMENTO DE SER DECLARADOS>>. SI SE LES ENVIA VALORES,
+<<DEBE UTILIZAR LOS RECIBIDOS>>, EN CASO CONTRARIO DEBE
+INICIALIZARLAS EN 01/01/1900.
+*/
+
+void funcionAlPepe(){
+    Contador obj;
+}
+
+/**
+HACER UN PROGRAMA CON UN MENU QUE TENGA LAS SIGUIENTES OPCIONES:
+-AGREGAR UN ALUMNO.
+-LISTAR TODOS LOS ALUMNOS.
+CADA OPCION DEL MENU DEBE LLAMAR A UNA FUNCION (NO HACER TODO EN EL
+                                                MAIN)
+PUEDE USARSE MAS DE UNA FUNCION PARA CARGAR Y/O MOSTRAR. TRATAR DE
+HACERLO LO MAS MODULAR POSIBLE.
+*/
+
+int main(){
+    setlocale(LC_ALL, "spanish");
+    FILE *p;
+    p = fopen("Alumnos.dat", "rb");///new
+    /**
+    fopen es una función que crea un vínculo entre mi programa
+    y el archivo. Este vínculo se guarda en el puntero FILE.
+    Los modos de apertura son:
+    -rb: Abre el archivo en modo lectura. NO crea archivos.
+    -ab: Abre el archivo en modo escritura. Si no existe, lo crea.
+        Siempre escribe al final del archivo.
+    -wb: Abre el archivo en modo escritura. SIEMPRE crea un archivo
+        vacío.
+    */
+    if(p == nullptr){
+        cout<<"ERROR AL ABRIR EL ARCHIVO"<<endl;
+        return -1;
+    }
+    ///USO EL ARCHIVO
+    Alumno alu;
+    while(fread(&alu, sizeof (Alumno), 1, p) == 1){
+        alu.Mostrar();
+        cout<<endl;
+    }
+//    fread(alu, sizeof (Alumno), 2, p);
+//    alu[0].Mostrar();
+//    cout<<endl;
+//    alu[1].Mostrar();
+//    cout<<endl;
+//    fread(alu, sizeof (Alumno), 2, p);
+//    alu[0].Mostrar();
+//    cout<<endl;
+//    alu[1].Mostrar();
+//    cout<<endl;
+    /**
+    fread copia de mi archivo la cantidad de bytes que surge de
+    multiplicar el segundo y el tercer parámetro entre si y lo
+    guarda en la dirección de memoria del primer parámetro.
+    -El primer parámetro es la dirección de memoria donde
+    quiero copiar la información.
+    -El segundo es el tamaño de UN registro.
+    -El tercero es la cantidad de registros que quiero leer
+    -El cuarto es el vínculo con el archivo (puntero FILE).
+    Lee hasta el end of file (eof). Si estoy en el eof, no lee nada
+    y no modifica la información de la memoria.
+    */
+    ///alu.Cargar();
+    ///fwrite(&alu, sizeof(Alumno), 1, p);
+    /**
+    fwrite copia de la dirección de memoria del primer parámetro
+    la cantidad de bytes que surge de multiplicar el segundo
+    y el tercer parámetro entre si y lo guarda en el archivo.
+    -El primer parámetro es la dirección de memoria desde donde
+    quiero copiar la información.
+    -El segundo es el tamaño de UN registro.
+    -El tercero es la cantidad de registros que quiero guardar
+    -El cuarto es el vínculo con el archivo (puntero FILE).
+    */
+    fclose(p); ///delete[] p;
+    return 0;
+    Contador obj;
+    //obj.establecerValorInicial(5);
+    obj.Mostrar();
+    obj.incrementar();
+    obj.incrementar();
+    obj.incrementar(5);
+    obj.Mostrar();
+    funcionAlPepe();
+    obj.decrementar();
+    obj.decrementar();
+    obj.decrementar();
+    obj.decrementar();
+    obj.decrementar();
+    obj.Mostrar();
+    obj.reiniciar();
+    obj.Mostrar();
+    return 0;
+    menu();
+    return 0;
+}
